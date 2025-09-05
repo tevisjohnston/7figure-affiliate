a7d1235107bf97fb885b9dfd242a6c365c0f0cb0
Tevis <tevisjohnston@gmail.com>
2025-09-05 11:26:33 -0700

    Added pages +images.

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '7figure-affiliate' );
+define( 'DB_NAME', '7figure-affiliate-edits-2' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
